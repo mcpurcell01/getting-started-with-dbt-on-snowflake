016b8a6574c13bd42b8237de94185be05e581f5e
mcpurcell01 <mcpurcell@yahoo.com>
2025-08-21 11:03:48 -0400

    Delete Vending_Transactions/target directory

--- Vending_Transactions/target/compiled/vending_machine_dbt/models/staging/raw_vending_machines.sql
+++
@@ -1,7 +0,0 @@
--- This staging model selects all columns from the raw vending machines table.
-SELECT
-    vending_machine_id,
-    location_city,
-    region,
-    country
-FROM INSTRUCTOR1_vending_machine_dbt_db.RAW.VENDING_MACHINES
